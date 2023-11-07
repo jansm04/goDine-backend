6b0f693d0cfb8f3c7d2779953d059909a01ddd88
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-06 22:37:54 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+const { callAPI } = require('./api-call');
+
+// express app
+const app = express();
+app.use(cors());
+app.use(express.json()); // middleware to parse data
+
+app.get('/api/call', async (req, res) => {
+    const mood = req.query.mood;
+    try {
+        const data = await callAPI(mood);
+        res.json({ data });
+    } catch (error) {
+        res.status(500).json({ error: `Error: ${error}`});
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+})
+
+
