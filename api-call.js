ae15b51e76bcc50f78fec4a8651c3408b91e1474
jansmailbegovic <jan.smailbegovic@gmail.com>
2023-11-07 22:01:30 -0800

    parsed openai data into string array

--- api-call.js
+++ api-call.js
@@ -6,16 +6,26 @@ console.log(openAI_key);
 
 const openai = new openAI({ apiKey: openAI_key });
 
+
 async function callAPI(type, mood) {
     try {
         const response = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [{
                 "role": "user", 
-                "content": `List the 5 best ${mood} ${type} restaurants in Toronto. Include names only.`,
+                "content": `Return one line of text consisting of the 5 best ${mood} ${type} restaurants in Toronto. 
+                            Include names only, each separated by a '#'.`,
             }]
           });
-        return response.choices[0].message.content;
+          // create array of restaurant names
+          const text = response.choices[0].message.content;
+          const words = text.split('#');
+
+          for (let i = 0; i < words.length; i++) {
+            console.log(`${i+1}. ${words[i]}`);
+          }
+
+        return words;
     } catch (error) {
         console.log("An error occurred fetching the OpenAI response.");
         return error;
